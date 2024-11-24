8bd8592dc718db1ac949156dc888ff8640ec6553
randelj <randelreeder04@gmail.com>
2024-11-24 14:33:17 +0200

    Sõnavara koos templatega

--- javascript.js
+++ javascript.js
@@ -1,54 +1,50 @@
-function scrollFooter(scrollY, heightFooter)
-{
+function scrollFooter(scrollY, heightFooter) {
     console.log(scrollY);
     console.log(heightFooter);
 
-    if(scrollY >= heightFooter)
-    {
+    if (scrollY >= heightFooter) {
         $('footer').css({
-            'bottom' : '0px'
+            'bottom': '0px'
         });
-    }
-    else
-    {
+    } else {
         $('footer').css({
-            'bottom' : '-' + heightFooter + 'px'
+            'bottom': '-' + heightFooter + 'px'
         });
     }
 }
 
-$(window).load(function(){
-    var windowHeight        = $(window).height(),
-        footerHeight        = $('footer').height(),
-        heightDocument      = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
+$(window).on('load', function () {
+    var windowHeight = $(window).height(),
+        footerHeight = $('footer').height(),
+        heightDocument = windowHeight + $('.content').height() + $('footer').height() - 20;
 
-    // Definindo o tamanho do elemento pra animar
+    // Set height for the scrollable container
     $('#scroll-animate, #scroll-animate-main').css({
-        'height' :  heightDocument + 'px'
+        'height': heightDocument + 'px'
     });
 
-    // Definindo o tamanho dos elementos header e conteúdo
+    // Set header and content dimensions
     $('header').css({
-        'height' : windowHeight + 'px',
-        'line-height' : windowHeight + 'px'
+        'height': windowHeight + 'px',
+        'line-height': windowHeight + 'px'
     });
 
     $('.wrapper-parallax').css({
-        'margin-top' : windowHeight + 'px'
+        'margin-top': windowHeight + 'px'
     });
 
     scrollFooter(window.scrollY, footerHeight);
 
-    // ao dar rolagem
-    window.onscroll = function(){
+    // On scroll event
+    window.onscroll = function () {
         var scroll = window.scrollY;
 
         $('#scroll-animate-main').css({
-            'top' : '-' + scroll + 'px'
+            'top': '-' + scroll + 'px'
         });
-        
+
         $('header').css({
-            'background-position-y' : 50 - (scroll * 100 / heightDocument) + '%'
+            'background-position-y': 50 - (scroll * 100 / heightDocument) + '%'
         });
 
         scrollFooter(scroll, footerHeight);
