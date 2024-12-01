fceebd523967d88eeea0cda967be7fe98dae3e2d
scheler1 <scheler@ut.ee>
2024-12-01 13:12:49 +0200

    villem

--- javascript.js
+++ javascript.js
@@ -1,22 +1,25 @@
-$(window).on('load', function () {
-    var windowHeight = $(window).height();
+// VIITAMINE - JAVASCRIPT ON KIRJUTATUD CHATGPT AI POOLT.
 
-    // Set header and content dimensions
-    $('header').css({
-        'height': windowHeight + 'px',
-        'line-height': windowHeight + 'px'
-    });
-
-    $('.wrapper-parallax').css({
-        'margin-top': windowHeight + 'px'
-    });
-
-    // Parallax effect on scroll
-    window.onscroll = function () {
-        var scroll = window.scrollY;
-
-        $('header').css({
-            'background-position-y': 50 - (scroll * 50 / windowHeight) + '%'
-        });
-    };
+$('header').css({
+    'height': windowHeight + 'px', // Headeri kõrgus = akna kõrgus.
+    'line-height': windowHeight + 'px' // Tekst joondatakse vertikaalselt keskele.
 });
+
+// Nihutame parallax-sisu algust.
+$('.wrapper-parallax').css({
+    'margin-top': windowHeight + 'px' // Nihkub täpselt ühe akna kõrguse võrra allapoole.
+});
+
+// Seadistame sündmuse, mis käivitub lehe kerimisel.
+window.onscroll = function () { 
+    var scroll = window.scrollY; 
+    // Salvestame lehe kerimise kauguse ülevalt servast pikslites.
+
+    // Muudame headeri taustapildi positsiooni vertikaalselt, et luua parallax-efekt.
+    $('header').css({
+        'background-position-y': 50 - (scroll * 50 / windowHeight) + '%' 
+        // Arvutus:
+        // 50% - Tausta algne positsioon (keskkoht).
+        // (scroll * 50 / windowHeight) - Liikumise kiirus, mis sõltub kerimise kaugusest ja akna kõrgusest.
+    });
+};
